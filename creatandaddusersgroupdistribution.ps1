48612b6557b8ccc07edf18204a2759809be437f4
aacavalcante <39135039+aacavalcante@users.noreply.github.com>
2019-07-04 12:18:13 -0300

    Add files via upload

---
+++ creatandaddusersgroupdistribution.ps1
@@ -0,0 +1,34 @@
+#use suas credenciais
+$credential = Get-Credential
+
+#importa a session do exchange online
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+
+#lista todos os grupos de distribuição
+Get-DistributionGroup
+
+#criar multiplos listas de distribuição com CSV
+$arquivo = Import-Csv “C:\Temp\xxxx.csv” -Delimiter ";"
+foreach ($item in $arquivo) {
+
+    $name = $item.name
+    $type = $item.type
+    New-DistributionGroup -Name $name -Type $type
+    Write-Host "Result:$name"
+
+}
+
+#adiciona multiplos usuários na lista pelo CSV
+$arquivo = Import-Csv D:\Temp\xxxx.csv -Delimiter ";"
+foreach ($item in $arquivo) {
+
+    $grupos = $item.grupos
+    $user = $item.user
+    Add-DistributionGroupMember -Identity $grupos -Member $user
+    Write-Host "Result:$user"
+
+}
+
+#adiciona um único usuário
+Add-DistributionGroupMember -Identity "Name for Group" -Member "email for user" -BypassSecurityGroupManagerCheck
